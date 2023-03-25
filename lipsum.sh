0d4c4adff7ee7de2afe70ef468af2c1d9fa99d64
ebarrosv <radon63@gmail.com>
2023-03-24 22:00:23 -0300

    3d commit

--- lipsum.sh
+++ lipsum.sh
@@ -2,11 +2,11 @@
 
 for i in {1..5}
 do
-    #obtiene una cantidad aleatoria de número de lineas entre 1 y 20
+    #get a random line number between 1 and 20
     lineas=$(shuf -i 1-20 -n 1)
-    #llama con curl a lipsum.com y le pasa como parámetro la cantidad de líneas
+    #we call curl using limpsum.com and take as argument the number of lines
     lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
 )   
-    #crea un archivo con el contenido de la variable lorem
+    #creates a new file including the content of the variable lorem
     echo "$lorem" >> loremipsum-$i.txt
 done
