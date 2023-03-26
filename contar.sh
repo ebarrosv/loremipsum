9e619e4339c4e847a6145965a360af94f3b88bc6
ebarrosv <radon63@gmail.com>
2023-03-26 00:18:06 -0300

    Comments added

--- contar.sh
+++ contar.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#This variable definition filters the amount of files that starts with "lorem"
 lorems=$(ls |grep lorem |wc -l)
 
 for i in $lorems
 do
+    #This variable "amount" stores the quantity of lines
     amount=$(wc -l loremipsum-*)
+
+    #Here we parse the output of "wc" stored in "amount", to match the desired output as the specifications
+    #dictate, first comes the filename, second the amount of lines, and "head" takes only the first $i lines
     name=`echo "$amount" | awk '{print $2 " tiene " $1 " lineas" }' | head -$i`
+    
     printf "$name \n"
 done
--- lipsum.sh
+++ lipsum.sh
@@ -2,11 +2,11 @@
 
 for i in {1..5}
 do
-    #get a random line number between 1 and 20
+    #Gets a random line number between 1 and 20
     lineas=$(shuf -i 1-20 -n 1)
-    #we call curl using limpsum.com and take as argument the number of lines
+    #We call curl using limpsum.com and take as argument the number of lines
     lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
 )   
-    #creates a new file including the content of the variable lorem
+    #Creates a new file including the content of the variable lorem
     echo "$lorem" >> loremipsum-$i.txt
 done
