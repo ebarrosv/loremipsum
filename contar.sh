4f5d1697f925ba5f9b94bf229a1a7091d3a2655d
ebarrosv <radon63@gmail.com>
2023-03-26 00:04:18 -0300

    output parsed correctly according to specifications

--- contar.sh
+++ contar.sh
@@ -5,6 +5,6 @@ lorems=$(ls |grep lorem |wc -l)
 for i in $lorems
 do
     amount=$(wc -l loremipsum-*)
-    name=`echo "$amount" | awk '{print $2 " tiene " $1 " lineas " }' | head -$i`
-    echo $name
+    name=`echo "$amount" | awk '{print $2 " tiene " $1 " lineas" }' | head -$i`
+    printf "$name \n"
 done
